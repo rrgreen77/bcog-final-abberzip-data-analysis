b5640f3431228293e3d3ff1bb42f43d3c6ab03dd
rrgreen77 <rileyeg2@illinois.edu>
2025-04-11 15:16:26 -0500

    Update and rename test.md to test.py

---
+++ test.py
@@ -0,0 +1,2 @@
+#there will be no input data for the user to handle, as it would all be analyzed by me and presented in the Jupyter notebook. 
+#the Jupyter notebook will be displayed on my Github. 
